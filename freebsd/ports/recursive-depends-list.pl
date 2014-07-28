2f504480176eb8be097d9922ba73a25419ee1910
Byron Grobe <grobe0ba@gmail.com>
2014-07-28 00:55:25 +0000

    dc1a2902-15f1-11e4-93c3-0015178af5da

---
+++ freebsd/ports/recursive-depends-list.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+
+use strict;
+use Data::Dumper;
+
+my $depth=0;
+my $prefix="";
+my %portindex=();
+
+sub getdep
+{
+	(my $KEY,undef)=@_;
+
+	my @DEPS=split(/ /, $portindex{"$KEY"});
+
+	foreach(@DEPS)
+	{
+		$prefix=~tr/[ |]/ /c;
+		$prefix="$prefix|--";
+		print "$prefix$_\n";
+		getdep($_);
+		$prefix=""
+	}
+}
+
+
+open(my $INDEX_FILE, "< /usr/ports/INDEX-10") or die "Could not open INDEX file: $!\n";
+
+while(defined(my $LINE=<$INDEX_FILE>))
+{
+	chomp $LINE;
+	my @PORT = split(/\|/, $LINE);
+	my $KEY=$PORT[0];
+	my $VALUE="$PORT[7] $PORT[8]";
+
+	$portindex{"$KEY"}=$VALUE;
+}
+
+if($ARGV[0])
+{
+	getdep($ARGV[0]);
+}
