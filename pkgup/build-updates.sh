b7faf47c3feb1b6d3c41f5d8b865f0280ca751eb
Byron Grobe <grobe0ba@gmail.com>
2014-09-03 17:29:50 -0400

    6f8d1953-33b1-11e4-8f86-001517fe4aa0

---
+++ pkgup/build-updates.sh
@@ -0,0 +1,22 @@
+#!/usr/local/bin/bash
+
+HOSTNAMES[0]="valhalla"
+HOSTNAMES[1]="asgard"
+
+JAIL="build10"
+PORTS="default"
+
+rm *.pkglist pkglist
+
+declare -a HOSTNAME
+
+for HOST in $(seq 0 $((${#HOSTNAMES[@]} - 1)));
+do
+	ssh $HOST pkg query -a "%o" > $HOSTNAME.pkglist
+end
+
+cat *.pkglist > pkglist
+
+sudo poudriere bulk -j $JAIL -p $PORTS -f pkglist
+
+rm *.pkglist
