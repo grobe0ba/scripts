e75ecf6941834ea04339231dbb2df18328522a80
Byron Grobe <grobe0ba@gmail.com>
2014-09-03 21:40:11 +0000

    e1d18efb-33b2-11e4-90ac-0015178af5da

--- pkgup/build-updates.sh
+++ pkgup/build-updates.sh
@@ -2,6 +2,8 @@
 
 HOSTNAMES="valhalla asgard"
 
+REMOVE_PACKAGES="sysutils/vagrant"
+
 JAIL="build10"
 PORTS="default"
 
@@ -16,6 +18,16 @@ do
 done
 
 cat *.pkglist > pkglist
+
+TMP=$(mktemp /tmp/pkgup.XXXX)
+
+for PKG in $REMOVE_PACKAGES;
+do
+	grep -v $PKG pkglist > $TMP
+	cp $TMP pkglist
+done
+
+rm $TMP
 
 sudo poudriere ports -u -p $PORTS
 sudo poudriere bulk -j $JAIL -p $PORTS -f pkglist
