c014bcd9e2720efb784813c561e42ca26308cf29
Byron Grobe <grobe0ba@gmail.com>
2014-10-16 04:03:16 +0000

    5b235299-54e9-11e4-9bb0-0015178af5da

--- freebsd/ports/rdld.pl
+++ freebsd/ports/rdld.pl
@@ -59,7 +59,7 @@ while(defined(my $LINE=<$INDEX_FILE>))
 
 if($ARGV[0] && $ARGV[1])
 {
-    open($out, "| tred | dot > $ARGV[1]") or die "Could not open output file: $!\n";
+    open($out, "> $ARGV[1]") or die "Could not open output file: $!\n";
     print $out "digraph A {\n";
     print $out "$prefix [label=\"$ARGV[0]\"];\n";
     getdep($ARGV[0]);
@@ -67,9 +67,11 @@ if($ARGV[0] && $ARGV[1])
 }
 else
 {
-    print "Usage: recursive-depends-list-dot.pl [port] [output.svg]\n";
+    print "Usage: recursive-depends-list-dot.pl [port] [output.gv]\n";
     print "port should be in the form port-version (e.g., subversion-1.8.10_3)\n";
     print "You can find this by moving to the port directory and running:\n";
     print "\tmake describe|cut -d'|' -f1\n";
     print "This script requires dot and tred from graphviz to be available.\n";
+    print "After running this script, run:\n";
+    print "\ttred output.gv | dot -Tsvg -o graph.svg\n";
 }
